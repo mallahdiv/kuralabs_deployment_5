fe9de2bd01d13296eef8390e2fb9c7ffb3f8ddfe
mallahdiv <you@example.com>
2022-11-16 20:24:25 -0500

    now

--- intTerraform/ALB.tf
+++ intTerraform/ALB.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group" "url-app" {
   name        = "url-app"
-  port        = 8000
+  port        = 5000
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = aws_vpc.app_vpc.id
